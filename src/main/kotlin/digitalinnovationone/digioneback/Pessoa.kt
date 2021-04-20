74d0f9235da602be6afacb96a70e9fc10a8832b5
Karen Mayumi <karenm.hirayama@gmail.com>
2021-04-20 12:56:17 -0300

    creat person class

---
+++ src/main/kotlin/digitalinnovationone/digioneback/Pessoa.kt
@@ -0,0 +1,15 @@
+package digitalinnovationone.digioneback
+
+class Pessoa {
+    var name: String = "Karen"
+    var cpf: String = "123.123.123 - 12"
+}
+
+fun main() {
+    val Karen = Pessoa()
+
+    println(Karen.name)
+
+    println(Karen.cpf)
+
+}
